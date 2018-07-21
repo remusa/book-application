d14b30e9b1ff8be049322b281561697652f20c26
R Sánchez <r@renems.com>
2018-07-21 11:56:01 -0500

    Finished login and registration

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
+import requests
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request, flash, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -20,22 +25,48 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+KEY = "DNmoNwfQbaJFsYerLF4A"
+isbn = "0765326353"
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    # Get form information
+    if request.method == "POST":
+        username = str(request.form.get("inputEmail"))
+        password = str(request.form.get("inputPassword"))
 
+        rowCount = int(db.execute(
+            "SELECT * FROM users WHERE username = :username", {"username": username}).rowcount)
 
-@app.route("/register")
-def register():
-    return "Registration page"
-
-
-@app.route("/login")
-def login():
-    return "Login page"
+        # register new user
+        if rowCount == 0:
+            db.execute("INSERT INTO users (username, password) VALUES (:username, :password)", {
+                "username": username, "password": password})
+            db.commit()
+            return render_template("success.html", message="You were successfully registered")
+        # log in existing user
+        elif rowCount == 1:
+            checkPassword = db.execute("SELECT password FROM users WHERE username = :username", {
+                "username": username}).fetchone()
+            # log in successful
+            if password == checkPassword.password:
+                if username in session:
+                    return render_template("success.html", message="You were successfully logged in")
+                else:
+                    return render_template("success.html", message="You were successfully logged in")
+            # wrong password
+            else:
+                return render_template("error.html", message="Wrong password")
 
 
 @app.route("/logout")
 def logout():
-    return "Logged out"
+    session.pop("user", None)
+    return redirect(url_for("index"))
+
+
+# GoodReads API
+# res = requests.get("https://www.goodreads.com/book/review_counts.json",
+#                    params={"key": KEY, "isbns": isbn})
+# print(res.json())
--- import.py
+++ import.py
@@ -1,7 +1,17 @@
-import requests
-res = requests.get("https://www.goodreads.com/book/review_counts.json",
-                   params={"key": "KEY", "isbns": "9781632168146"})
-print(res.json())
+# Import: Provided for you in this project is a file called books.csv, which is a spreadsheet in CSV format of 5000 different books.
+# Each one has an ISBN nubmer, a title, an author, and a publication year.
+# In a Python file called import.py separate from your web application, write a program that will take the books and import them into your PostgreSQL database.
+# You will first need to decide what table(s) to create, what columns those tables should have, and how they should relate to one another.
+# Run this program by running python3 import.py to import the books into your database, and submit this program with the rest of your project code.
 
-key: DNmoNwfQbaJFsYerLF4A
-secret: ukaJG4jIHDu8b5v0wCs4iFIoVEOD5On8WLEJLB3z8
+import os
+import csv
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+if not os.getenv("DATABASE_URL"):
+    reaise RuntimeError("DATABASE_URL is not set")
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
