087faa8ecc12558f5c40628edaad2097ee5c5376
R Sánchez <rene.as.himself@gmail.com>
2018-10-29 20:43:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -175,8 +175,8 @@ def goodreads(isbn):
     return res.json()['books'][0]
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get('PORT', 5000))
+    # app.run(host='0.0.0.0', port=port)
     # app.run(debug=True)
